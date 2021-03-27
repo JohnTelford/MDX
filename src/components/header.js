a49bd0dd7b31eda43d629e66dc702530dbe040e8
John <john@johntelford.com>
2021-03-27 09:25:42 -0700

    MDX Test base commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from 'react'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'blue',
+      background: 'green',
       marginBottom: '1.45rem',
     }}
   >
